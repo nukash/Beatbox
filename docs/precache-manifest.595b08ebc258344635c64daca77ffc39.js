9210365e344c80f29794e7188c6b12ddc3d04a13
nukash <ryohei.nukaga@gmail.com>
2020-02-06 14:36:23 +0900

    minimum PWAnized

---
+++ docs/precache-manifest.595b08ebc258344635c64daca77ffc39.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a380977e6d022603fdab",
+    "url": "css/app.578ae515.css"
+  },
+  {
+    "revision": "212e223aef8c1514e1ab39d197d765f3",
+    "url": "images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "1921613cd35217bb39c3980ea796d6a5",
+    "url": "images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "e5a5b7eaa666719440ac1cf49c350fcf",
+    "url": "images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "7ab91c46b1127210ef45c6b2ee481553",
+    "url": "images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "c446f13f82e5902dd113ad1cf895f21e",
+    "url": "images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "65ff9c43aa76295ea42b7d78ff3f752e",
+    "url": "images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "f1ce9539d99939a7aa8d365615240c8d",
+    "url": "images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "a7aed13ad5506ec4ad2535647216c357",
+    "url": "images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bee162f89a0f5012662fd42435fb5421",
+    "url": "index.html"
+  },
+  {
+    "revision": "a380977e6d022603fdab",
+    "url": "js/app.c1ede791.js"
+  },
+  {
+    "revision": "083f825bd7b63ea74c70",
+    "url": "js/chunk-vendors.5006c987.js"
+  },
+  {
+    "revision": "d5e2d2255d4b1cdf95c7dc98c62956a4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
