6577f2993b17943bf05b6ab998825b38ce24c063
nukash <ryohei.nukaga@gmail.com>
2020-02-07 22:24:10 +0900

    BGM

---
+++ docs/precache-manifest.5e716d7d3eca72bf1489aa57a77b399b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a83ba7f3471f44a108c54697111fe",
+    "url": "audio/bgm/01/shuffle_shuffle.mp3"
+  },
+  {
+    "revision": "37f79662682cbea4ca9ae9308aa08989",
+    "url": "audio/bgm/02/日曜の午後.mp3"
+  },
+  {
+    "revision": "dd5477cb16eb25f416bd9ff200265a6d",
+    "url": "audio/bgm/03/ぽかん.mp3"
+  },
+  {
+    "revision": "42891b17159d64eb583afcda44006ff4",
+    "url": "audio/bgm/04/n74.mp3"
+  },
+  {
+    "revision": "a6e17810690770bac4ca0b8e5db3f16c",
+    "url": "audio/bgm/05/夏はsummer!!.mp3"
+  },
+  {
+    "revision": "014f61bd7dd2da24122973226e99bed8",
+    "url": "audio/bgm/06/ぐだぐだな感じ.mp3"
+  },
+  {
+    "revision": "c0ac1963474452dd6dc3fe9fe0db0ed7",
+    "url": "audio/bgm/07/パーティーはハチャメチャ大騒ぎ.mp3"
+  },
+  {
+    "revision": "7f81ff9a3c2a2fe2505d2f9bc6311f02",
+    "url": "audio/bgm/08/モーツァルト「Dies_irae」.mp3"
+  },
+  {
+    "revision": "5f58e1c22a6cf20ff77ff59864d069a7",
+    "url": "audio/bgm/09/Crystal_thorn.mp3"
+  },
+  {
+    "revision": "a1d5eb301f7bec89a8005947f0ac8c40",
+    "url": "audio/bgm/なんでしょう？.mp3"
+  },
+  {
+    "revision": "9f3365700bb199f1b66d43fa5b37a858",
+    "url": "audio/se/01/drum-japanese2.mp3"
+  },
+  {
+    "revision": "aff903e0d35b153f05be834284670918",
+    "url": "audio/se/02/trumpet1.mp3"
+  },
+  {
+    "revision": "544cedec590097c0cd6876880abd666b",
+    "url": "audio/se/03/kotsudumi1.mp3"
+  },
+  {
+    "revision": "33cc354d977a291867b969d3d3ca4512",
+    "url": "audio/se/04/tin1.mp3"
+  },
+  {
+    "revision": "e2b6a29b32d8dd2f9a5ce500e55d9d81",
+    "url": "audio/se/05/costume-drama1.mp3"
+  },
+  {
+    "revision": "7b224f8209cb4e8b3797431e8915a240",
+    "url": "audio/se/06/boyon1.mp3"
+  },
+  {
+    "revision": "ccd90db1cf981b3e2adbf4b08ac2ee25",
+    "url": "audio/se/07/Motion-Fracture03-1.mp3"
+  },
+  {
+    "revision": "b4a2d27d8352f97ffaa3f86b52c9c3c1",
+    "url": "audio/se/08/Cut04-1.mp3"
+  },
+  {
+    "revision": "c9091a60eea54b537e8778ffab66ed2e",
+    "url": "audio/se/09/tirin1.mp3"
+  },
+  {
+    "revision": "9ab54e638f4aa92b2a90",
+    "url": "css/chunk-vendors.0fc50112.css"
+  },
+  {
+    "revision": "212e223aef8c1514e1ab39d197d765f3",
+    "url": "images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "1921613cd35217bb39c3980ea796d6a5",
+    "url": "images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "e5a5b7eaa666719440ac1cf49c350fcf",
+    "url": "images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "7ab91c46b1127210ef45c6b2ee481553",
+    "url": "images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "c446f13f82e5902dd113ad1cf895f21e",
+    "url": "images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "65ff9c43aa76295ea42b7d78ff3f752e",
+    "url": "images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "f1ce9539d99939a7aa8d365615240c8d",
+    "url": "images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "a7aed13ad5506ec4ad2535647216c357",
+    "url": "images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "b5cd99e9c099f72efd95fd01124a4381",
+    "url": "index.html"
+  },
+  {
+    "revision": "a2b191d53d59a861fc1e",
+    "url": "js/app.484d8710.js"
+  },
+  {
+    "revision": "9ab54e638f4aa92b2a90",
+    "url": "js/chunk-vendors.69f89e31.js"
+  },
+  {
+    "revision": "46b7ae0262620d26baeac5e2c5000a23",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "5946ea5eb79e1a33c4de5d82cffce81c",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9f86b6522e792ea3ad29747b311821e8.js"
+  "precache-manifest.5e716d7d3eca72bf1489aa57a77b399b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "beatbox"});
